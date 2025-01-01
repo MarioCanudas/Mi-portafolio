bffe3755be00d7f0e61e6368bc5a0b55333dd9b3
MarioCanudas <mario.cz24@outlook.com>
2024-12-31 19:57:12 -0600

    feat: added short description and background

--- Portafolio/Portafolio.py
+++ Portafolio/Portafolio.py
@@ -26,71 +26,107 @@ class State(rx.State):
 
 def index() -> rx.Component:
     # Welcome Page (Index)
-    return rx.container(
+    return rx.box(
         
         rx.color_mode.button(position="bottom-right"),
         
-        rx.vstack(
+        rx.center(
             
-            rx.heading('Hola, soy Mario Canudas y este es mi portafolio.', 
-                size= '9'
-            ),
+            rx.vstack(
             
-            rx.hstack(
-            
-                rx.button(
-                    rx.hstack(
-                        rx.icon('github', color='white'),
-                        rx.markdown('GitHub', color='white'),
-                        align_items='center'
-                        ), 
-                    on_click= State.redirigir_github(),
-                    bg= 'black',
-                    color= 'white',
+                rx.box(
+                    
+                    rx.container(
+                        
+                        rx.vstack(
+                            
+                            rx.heading('Hola, soy Mario Canudas y este es mi portafolio.', 
+                                size= '8',
+                                align= 'center',
+                                color= 'black'
+                            ),
+                            
+                            rx.inset(
+                                rx.text(
+                                    'Soy estudiante de Actuaría, apasionado por el análisis de datos, los modelos de Machine Learning y la tecnología.',
+                                    align= 'center',
+                                    color= 'black'
+                                ),
+                                width="50vw",
+                                
+                            ),
+                            
+                            rx.hstack(
+                            
+                                rx.button(
+                                    rx.hstack(
+                                        rx.icon('github', color='white'),
+                                        rx.markdown('GitHub', color='white'),
+                                        align_items='center'
+                                    ), 
+                                    on_click= State.redirigir_github(),
+                                    bg= 'black',
+                                    color= 'white',
+                                ),
+                                
+                                rx.button(
+                                    rx.hstack(
+                                        rx.icon('linkedin', color='white'),
+                                        rx.markdown('LinkedIn', color='white'),
+                                        align_items='center'
+                                        ), 
+                                    on_click= State.redirigir_linkedin(),
+                                    bg= 'blue',
+                                    color= 'white',
+                                ),
+                                
+                                rx.button(
+                                    rx.hstack(
+                                        rx.icon(tag= 'mail', color='white'),
+                                        rx.markdown('Mail', color='white'),
+                                        align_items='center'
+                                    ), 
+                                    on_click= State.copiar_correo(),
+                                    bg= 'grey',
+                                    color= 'white',
+                                ),
+                                
+                                rx.button(
+                                    rx.hstack(
+                                        rx.icon(tag='phone', color='white'),
+                                        rx.markdown('WhatsApp', color='white'),
+                                        align_items='center'
+                                    ), 
+                                    on_click= State.redirigir_whatsapp(),
+                                    bg= 'green',
+                                    color= 'white',
+                                ),
+                                
+                            ),
+                            align= 'center',
+                            spacing= '4',
+                            
+                        ),
+                        align_items= 'center',
+                        justify_content= 'center',
+                        spacing= '5',
+                        min_height= '85vh',
+                        
+                    ),
+                    background_image= "url('/background_1st_part.jpg')",
+                    background_size= 'cover',
+                    background_position= 'center',
+                    height= "70vh",
+                    border_bottom_left_radius="10px",
+                    border_bottom_right_radius="10px"
+                    
                 ),
-                
-                rx.button(
-                    rx.hstack(
-                        rx.icon('linkedin', color='white'),
-                        rx.markdown('LinkedIn', color='white'),
-                        align_items='center'
-                        ), 
-                    on_click= State.redirigir_linkedin(),
-                    bg= 'blue',
-                    color= 'white',
-                ),
-                
-                rx.button(
-                    rx.hstack(
-                        rx.icon(tag= 'mail', color='white'),
-                        rx.markdown('Mail', color='white'),
-                        align_items='center'
-                        ), 
-                    on_click= State.copiar_correo(),
-                    bg= 'grey',
-                    color= 'white',
-                ),
-                
-                rx.button(
-                    rx.hstack(
-                        rx.icon(tag='phone', color='white'),
-                        rx.markdown('WhatsApp', color='white'),
-                        align_items='center'
-                        ), 
-                    on_click= State.redirigir_whatsapp(),
-                    bg= 'green',
-                    color= 'white',
-                )
-            
-            ),
-            
-            align_items= 'center',
-            spacing="5",
-            justify="center",
-            min_height="85vh"
-        ),
-    )
 
+            ),
+            
+        ), 
+        
+    )
 
 app = rx.App()
 app.add_page(index)
